125ee2df69ebbd7640abfdee7ccdd31f4d8c86c4
jejusee <44075062+jejusee@users.noreply.github.com>
2023-10-27 12:13:52 +0900

    Update ramdisk.sh

--- linux/ramdisk.sh
+++ linux/ramdisk.sh
@@ -8,8 +8,10 @@ size="8G"
 if [ "$1" == "install" ]; then
 
     # 사용자로부터 경로와 용량 입력 받기
-    read -p "Ramdisk를 설치할 경로를 입력하세요 (기본값: $path): " custom_path
+    read -p "Ramdisk를 설치할 위치를 입력하세요 (기본값: $path): " custom_path
     path=${custom_path:-$path}
+    read -p "Ramdisk의 용량을 입력하세요 (예: 100M, 1G, 기본값: $size): " custom_size
+    size=${custom_size:-$size}
     
     # /mnt 경로에만 설치 가능하도록 확인
     if [[ ! $path =~ ^/mnt/ ]]; then
@@ -17,7 +19,7 @@ if [ "$1" == "install" ]; then
         exit 1
     # 경로가 존재하지 않을 경우
     elif [ ! -d "$path" ]; then
-        read -p "\"$path\" 경로가 존재하지 않습니다. 새로 만드시겠습니까? (y/n): " response
+        read -p "\"$path\" 가 존재하지 않습니다. 새로 만드시겠습니까? (y/n): " response
         if [ "$response" != "y" ]; then
             echo "설치를 취소하였습니다."
             exit 1
@@ -31,14 +33,10 @@ if [ "$1" == "install" ]; then
             exit 1
         fi
         sudo umount $path
-    fi
-    
-    read -p "Ramdisk의 용량을 입력하세요 (예: 100M, 1G, 기본값: $size): " custom_size
-    size=${custom_size:-$size}
+    fi    
 
     # Ramdisk 생성
     sudo mount -t tmpfs -o size=$size tmpfs $path
-    echo "램디스크 설치하였습니다."
 
     # 생성된 Ramdisk 정보 출력
     df -h $path
@@ -46,7 +44,8 @@ if [ "$1" == "install" ]; then
     # /etc/fstab에 등록
     sudo sed -i "\~$path~d" /etc/fstab
     echo "tmpfs $path tmpfs size=$size 0 0" | sudo tee -a /etc/fstab > /dev/null
-    echo "시작시 램디스크를 사용합니다."
+
+    echo "램디스크 설치완료!! 시작시 자동실행합니다."
 
 # 삭제 모드일 때
 elif [ "$1" == "uninstall" ]; then
@@ -67,11 +66,11 @@ elif [ "$1" == "uninstall" ]; then
     # Ramdisk 삭제
     sudo umount $path
     sudo rm -rf $path
-    echo "Ramdisk가 삭제되었습니다."
 
     # /etc/fstab에서 삭제
     sudo sed -i "\~$path~d" /etc/fstab
-    echo "시작시 램디스크를 사용하지 않습니다."
+
+    echo "램디스크 삭제완료!! 시작시 사용하지 않습니다."
 else    
     script_name=$(basename "$0")    # 현재 스크립트 이름
     echo "사용법: ./$script_name [install | uninstall]"
