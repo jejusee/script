512f09f7b3c3269bbe6922453771275290db8fea
jejusee <44075062+jejusee@users.noreply.github.com>
2023-10-27 12:00:37 +0900

    Update ramdisk.sh

--- linux/ramdisk.sh
+++ linux/ramdisk.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# 기본값 설정
+path="/mnt/ramdisk"
+size="8G"
+
 # 설치 모드일 때
 if [ "$1" == "install" ]; then
-
-    # 기본값 설정
-    path="/ramdisk"
-    size="8G"
 
     # 사용자로부터 경로와 용량 입력 받기
     read -p "Ramdisk를 설치할 경로를 입력하세요 (기본값: $path): " custom_path
     path=${custom_path:-$path}
-
+    
+    # /mnt 경로에만 설치 가능하도록 확인
+    if [[ ! $path =~ ^/mnt/ ]]; then
+        echo "Ramdisk는 /mnt 경로에만 설치할 수 있습니다."
+        exit 1
     # 경로가 존재하지 않을 경우
-    if [ ! -d "$path" ]; then
+    elif [ ! -d "$path" ]; then
         read -p "\"$path\" 경로가 존재하지 않습니다. 새로 만드시겠습니까? (y/n): " response
         if [ "$response" != "y" ]; then
             echo "설치를 취소하였습니다."
@@ -49,11 +53,17 @@ elif [ "$1" == "uninstall" ]; then
 
     # 현재 설치된 Ramdisk 목록 출력
     echo "현재 설치된 Ramdisk 목록:"
-    df -h | grep tmpfs
+    df -h | grep ^tmpfs | grep -e '/mnt/'
 
     # 사용자로부터 삭제할 Ramdisk 선택 받기
     read -p "삭제할 Ramdisk의 경로를 입력하세요: " path
-
+    
+    # /mnt 경로에서만 삭제 가능하도록 확인
+    if [[ ! $custom_path =~ ^/mnt/ ]]; then
+        echo "Ramdisk는 /mnt 경로에서만 삭제할 수 있습니다."
+        exit 1
+    fi
+    
     # Ramdisk 삭제
     sudo umount $path
     sudo rm -rf $path
